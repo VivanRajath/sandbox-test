3707eb60211b5bbe01540ad6d93f1e3b390c72fc
VivanRajath <vivanrajath999@gmail.com>
2026-03-28 23:58:06 +0530

    Edited server.js based on junior code edit output

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req,res)=>{
-    res.send("Sandbox Test App Running")
+    res.send("code edited")
 })
 
 app.listen(3000, ()=>{
